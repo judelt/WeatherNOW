4c40bed0fc2d201bf81a19ba29c669c0cee84f07
Judit Mendez Puig <mendez.puig@gmail.com>
2021-05-14 02:49:53 +0000

    create helpers

--- weather_app/src/helpers/helpers.js
+++ weather_app/src/helpers/helpers.js
@@ -1,4 +1,6 @@
-export function dateBuilder (d) {
+import moment from 'moment'
+
+export function dateBuilder(d) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -8,4 +10,10 @@ export function dateBuilder (d) {
   let year = d.getFullYear();
 
   return `${day} ${date} ${month} ${year}`
+}
+
+export function localTime (t) {
+  const timezoneInMinutes = t/ 60;
+  const currTime = moment().utcOffset(timezoneInMinutes).format("h:mm A");
+  return currTime;
 }
