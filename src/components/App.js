ffc6c7513d8aa5c5df0e54eb31e0f2bd06392955
judelt <mendez.puig@gmail.com>
2021-05-26 16:35:04 -0700

    linted

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 import classnames from "classnames";
-import { useState } from 'react';
+import { useState } from "react";
 import { dateBuilder, localTime } from "../helpers/helpers";
 
 const api = {
   key: "b81405fabcf783e25e2cc3bedd1e9d3e",
-  base: "https://api.openweathermap.org/data/2.5"
+  base: "https://api.openweathermap.org/data/2.5",
 };
-
 
 function App() {
   const [query, setQuery] = useState("");
@@ -19,87 +18,84 @@ function App() {
   const search = (e) => {
     if (e.key === "Enter") {
       // https://openweathermap.org/current
-      axios.get(`${api.base}/weather?q=${query}&appid=${api.key}`)
+      axios
+        .get(`${api.base}/weather?q=${query}&appid=${api.key}`)
         .then((res) => {
           setWeather(res.data);
           setTemp(Math.round(res.data.main.temp - 273.15));
-          setTimezone(res.data.timezone)
-          setIcon(res.data.weather[0].icon)
+          setTimezone(res.data.timezone);
+          setIcon(res.data.weather[0].icon);
           setQuery("");
-      }) 
+        });
     }
-  }
-  
+  };
+
   const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
   const weatherClass = classnames("app", {
     "extreme-warm": temp >= 30,
-    "warm": temp < 30 && temp >= 20,
-    "medium": temp < 20 && temp >= 12,
-    "cold": temp < 12 && temp >= -5,
+    warm: temp < 30 && temp >= 20,
+    medium: temp < 20 && temp >= 12,
+    cold: temp < 12 && temp >= -5,
     "very-cold": temp < -5 && temp >= -20,
     "extreme-cold": temp < -20,
   });
 
-  
   return (
     <div className={weatherClass}>
-     <main>
-      <header className="header">
-      <h1>weather<span>NOW</span></h1>  
-      </header>
-      
-      { weather.main ? (
+      <main>
+        <header className="header">
+          <h1>
+            weather<span>NOW</span>
+          </h1>
+        </header>
+
+        {weather.main ? (
           <>
-        <div className="search-box">
-          <input
-              type="text"
-              className="search-bar"
-              placeholder="Search for city..."
-              onChange={e => setQuery(e.target.value)}
-              value={query}
-              onKeyPress={search} 
-          />
-        </div>
-        <div className="location-box">
-          <div className="location">{ weather.name }, {weather.sys.country}</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          
-          <div className="temp">
-            {Math.round(temp)}°C
-          </div>
-            
-          <div className="weather animate">  
-            <img src={ iconURL} alt='icon'></img>
-          </div>
-          
-          <div className="date">
-            {localTime(timezone)}
-          </div>
-              
-        </div>
-      </>
+            <div className="search-box">
+              <input
+                type="text"
+                className="search-bar"
+                placeholder="Search for city..."
+                onChange={(e) => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+              />
+            </div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(temp)}°C</div>
+
+              <div className="weather animate">
+                <img src={iconURL} alt="icon"></img>
+              </div>
+
+              <div className="date">{localTime(timezone)}</div>
+            </div>
+          </>
         ) : (
-        <>
-        <section className="question">
-         <h1>What's the weather like in...?</h1>     
-        </section> 
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search for city..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search} 
-          />
-        </div>   
-        </>
-      )}
-        
-     </main>
+          <>
+            <section className="question">
+              <h1>What's the weather like in...?</h1>
+            </section>
+            <div className="search-box">
+              <input
+                type="text"
+                className="search-bar"
+                placeholder="Search for city..."
+                onChange={(e) => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+              />
+            </div>
+          </>
+        )}
+      </main>
     </div>
   );
 }
--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,19 +1,40 @@
-import moment from 'moment'
+import moment from "moment";
 
 export function dateBuilder(d) {
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   let day = days[d.getDay()];
   let date = d.getDate();
   let month = months[d.getMonth()];
   let year = d.getFullYear();
 
-  return `${day} ${date} ${month} ${year}`
+  return `${day} ${date} ${month} ${year}`;
 }
 
-export function localTime (t) {
-  const timezoneInMinutes = t/ 60;
+export function localTime(t) {
+  const timezoneInMinutes = t / 60;
   const currTime = moment().utcOffset(timezoneInMinutes).format("h:mm A");
   return currTime;
 }
